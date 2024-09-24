a82251343ed8d69b0eac67e00eb31b9e6cc7e907
ShobhkarTiwari <st.shubh.tiwari@gmail.com>
2024-09-24 13:09:41 -0500

    Initial Commit

---
+++ FlickerApp/FlickerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerAppApp.swift
+//  FlickerApp
+//
+//  Created by Shobhakar Tiwari on 9/24/24.
+//
+
+import SwiftUI
+
+@main
+struct FlickerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FlickerAppTests/FlickerAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  FlickerAppTests.swift
+//  FlickerAppTests
+//
+//  Created by Shobhakar Tiwari on 9/24/24.
+//
+
+import Testing
+@testable import FlickerApp
+
+struct FlickerAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
